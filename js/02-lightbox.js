b80ff21f8ddc750bb3f4974baf1a099cdf4cf37f
77Vadim77 <125355511+77Vadim77@users.noreply.github.com>
2023-08-22 02:18:50 +0300

    update

---
+++ js/02-lightbox.js
@@ -0,0 +1,23 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const galleryMarcup = createGalleryMarcup(galleryItems);
+function createGalleryMarcup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) =>{
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>`;
+    })
+    .join("");
+}
+gallery.insertAdjacentHTML("beforeend", galleryMarcup);
+let lightBox = new SimpleLightbox ('.gallery a', {
+    captions: 'true',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
